5f334d74f2c67f0bb632df8db2e9caf38b8ad4a9
Herfj <54822117+Herfj@users.noreply.github.com>
2020-12-29 11:43:34 -0300

    ya tu sabebebebbe

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -77,6 +77,11 @@ function RenderDish(props) {
 		else return false;
 	};
 
+	const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+		if (dx < 200) return true;
+		else return false;
+	};
+
 	const panResponder = PanResponder.create({
 		onStartShouldSetPanResponder: (e, gestureState) => {
 			return true;
@@ -88,7 +93,8 @@ function RenderDish(props) {
 		},
 		onPanResponderEnd: (e, gestureState) => {
 			console.log("pan responder end", gestureState);
-			if (recognizeDrag(gestureState))
+
+			if (recognizeDrag(gestureState)) {
 				Alert.alert(
 					"Add Favorite",
 					"Are you sure you wish to add " + dish.name + " to favorite?",
@@ -107,6 +113,10 @@ function RenderDish(props) {
 					],
 					{ cancelable: false }
 				);
+			}
+			if (recognizeComment(gestureState)) {
+				props.onPress1();
+			}
 
 			return true;
 		},
